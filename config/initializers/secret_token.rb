681ba8186b4161802aa69ce0032e539709f0731f
Tony Ho <thetonyho@gmail.com>
2013-11-14 19:25:46 +1100

    rails new armaSearch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArmaSearch::Application.config.secret_key_base = 'bc58f3813d14a2c4a5681a2e601f26737145148573c8616badb85e51ce3dc2c571c2f47904ffa812e7167f49aee89554e621caa66ff5fdadac39f59bca85a8d2'
