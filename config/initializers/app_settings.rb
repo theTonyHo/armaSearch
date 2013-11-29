0e0423de6c0a61976813ac629d3869926c64483e
Tony Ho <thetonyho@gmail.com>
2013-11-29 12:36:54 +1100

    Clean

--- config/initializers/app_settings.rb
+++
@@ -1,2 +0,0 @@
-NAME = "armaSearch"
-SLOGAN = "Project Manager for the Construction Industry"
--- db/migrate/20131121093542_add_part_fields.rb
+++
@@ -1,7 +0,0 @@
-class AddPartFields < ActiveRecord::Migration
-  def change
-    add_column :parts, :number, :string
-    add_column :parts, :drawing, :integer
-    add_column :parts, :assembly, :integer
-  end
-end
